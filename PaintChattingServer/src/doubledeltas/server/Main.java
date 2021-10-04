5ad2fd29099e3dc9f4f694e3b006420a8471263f
doubledeltas <thesmartist@naver.com>
2021-10-04 11:36:34 +0900

    test

--- PaintChattingServer/src/doubledeltas/server/Main.java
+++ PaintChattingServer/src/doubledeltas/server/Main.java
@@ -9,17 +9,28 @@ todo:
 import java.sql.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static short MYSQL_DEFAULT_PORT = 3306;
+
+    public static Connection getConn(String address, short port, String user, String password)
+    {
+        Connection conn = null;
         try {
-            Class.forName("com.mysql.jdbc.Driver"); // MySQL 드라이버 로드
-            Connection conn = DriverManager.getConnection("jdbc:mysql:"
-                    + "localhost:3306/sampledb", "root", "password"
-                    );
-            System.out.println("DB 연결 완료");
-        } catch (ClassNotFoundException e) {
-            System.out.println("JDBC 드라이버 로드 에러");
-        } catch (SQLException e) {
-            System.out.println("DB 연결 에러");
+            Class.forName("com.mysql.cj.jdbc.Driver"); // MySQL 드라이버 로드
+            conn = DriverManager.getConnection(
+                    "jdbc:mysql://" + address + ":" + port,
+                    user, password);
+        } catch (Exception e) {}
+        return conn;
+    }
+
+    public static void main(String[] args)
+    {
+        Connection conn = getConn("localhost", MYSQL_DEFAULT_PORT, "root", "smartist2!");
+        if (conn != null) {
+            System.out.printf("DB 연결 완료");
+        }
+        else {
+            System.out.printf("DB 연결 에러");
         }
     }
 }

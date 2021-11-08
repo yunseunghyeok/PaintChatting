c32caf5e77c12420dc5422a0a3d3fa208405fdae
doubledeltas <thesmartist@naver.com>
2021-11-08 11:08:40 +0900

    6th?

--- PaintChattingServer/src/doubledeltas/server/CommandRouter.java
+++ PaintChattingServer/src/doubledeltas/server/CommandRouter.java
@@ -1,33 +1,38 @@
 package doubledeltas.server;
 
+import doubledeltas.utils.ByteStringReader;
 import doubledeltas.utils.TransferCode;
 
+import java.nio.charset.StandardCharsets;
+
 public class CommandRouter {
-	private int cur = 0;
 
 	public CommandRouter() {
 		// todo
 	}
-
-	public byte[] readBytesFrom(byte[] bytes, int count) {
-		int len = Math.min(count, bytes.length - cur);
-		if (len < 1) return null;
-
-		byte[] res = new byte[len];
-		for(int j=0; j<len; j++)
-			res[j] = bytes[cur+j];
-		cur += len;
-		return res;
-	}
 	
 	public boolean route(byte[] bytes) {
+		ByteStringReader bsr = new ByteStringReader(bytes);
+
+		String id, pw, nick, msg, oldNick;
+		int roomID;
 		switch (TransferCode
 				.valueOf(Byte.toString(bytes[0]))) {
-			case CHAT:
-				cur = 1;
-				String id = new String(readBytesFrom(bytes, 45));
-				String pw = new String(readBytesFrom(bytes, 45));
+			case LOGIN:
+				bsr.setCursor(1);
+				id		= bsr.readInString(45);
+				pw		= bsr.readInString(45);
 				break;
+			case REGISTER:
+				bsr.setCursor(1);
+				id		= bsr.readInString(45);
+				pw		= bsr.readInString(45);
+				nick 	= bsr.readInString(45);
+				break;
+			case ROOM_ENTER:
+				bsr.setCursor(1);
+				id		= bsr.readInString(45);
+				roomID	= bsr.readInInteger(4);
 			default:
 				break;
 		}
